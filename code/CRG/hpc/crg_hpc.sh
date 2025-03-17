1b3534bd4b1765e5a037a8e2c7fdc4c2f3dcd813
Ian S Jackson <isj0001@trcis001.hpc.wvu.edu>
2025-03-17 15:31:59 -0400

    finetune classifications

--- code/CRG/hpc/crg_hpc.sh
+++ code/CRG/hpc/crg_hpc.sh
@@ -9,9 +9,11 @@
 #SBATCH --gres=gpu:1
 #SBATCH --partition=comm_gpu_inter
 #SBATCH --mail-user=isj0001@mix.wvu.edu
+#SBATCH --mail-type=end
 
 module purge
 module load lang/gcc/12.2.0 lang/python/cpython_3.10.11_gcc122 parallel/cuda/11.8
 
 source /scratch/isj0001/AIAmbassador/code/CRG/venv/bin/activate
-python3 /scratch/isj0001/AIAmbassador/code/CRG/classify/finetune_transformer.py --BERT --force
+python3 /scratch/isj0001/AIAmbassador/code/CRG/classify/traditional_ML.py --SVM --force
+# python3 /scratch/isj0001/AIAmbassador/code/CRG/classify/finetune_transformer.py --BERT --force
